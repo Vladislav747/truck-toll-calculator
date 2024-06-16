78c1fcf04eafdfb5411d5555a4bb0471852401a4
vkudryakov <vkudryakov@ozon.ru>
2024-06-16 22:15:32 +0500

    Refactoring

--- invoicer/service/service.go
+++ invoicer/service/service.go
@@ -3,16 +3,19 @@ package service
 import (
 	"fmt"
 	"github.com/Vladislav747/truck-toll-calculator/types"
+	"github.com/sirupsen/logrus"
 )
 
 type Aggregator interface {
 	AggregateDistance(distance types.Distance) error
 	DistanceSum(int) (float64, error)
+	CalculateInvoice(int) (*types.Invoice, error)
 }
 
 type Storer interface {
 	Insert(types.Distance) error
 	Get(int) (float64, error)
+	Invoice(int) (*types.Invoice, error)
 }
 
 type InvoiceAggregator struct {
@@ -33,4 +36,20 @@ func (i *InvoiceAggregator) AggregateDistance(distance types.Distance) error {
 func (i *InvoiceAggregator) DistanceSum(obuId int) (float64, error) {
 	fmt.Println("calculating sum distance in the storage - obuId: ", obuId)
 	return i.store.Get(obuId)
+}
+
+func (i *InvoiceAggregator) CalculateInvoice(obuId int) (*types.Invoice, error) {
+	fmt.Println("calculating invoice - obuId: ", obuId)
+	dist, err := i.store.Get(obuId)
+	if err != nil {
+		logrus.Errorf("obu id (%d) not found", obuId)
+		return nil, fmt.Errorf("obu id (%d) not found", obuId)
+	}
+
+	inv := &types.Invoice{
+		OBUID:         obuId,
+		TotalDistance: dist,
+		TotalAmount:   0,
+	}
+	return inv, nil
 }
